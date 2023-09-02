0d9ed2a6f922334a6ebc7c0a35e44af9bb7c32b1
pilladipesh33 <pilladipesh.pd@gmail.com>
2023-09-02 11:49:21 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import Apps from './src/App';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <Apps />
+    </SafeAreaView>
+  );
+}
+
+export default App;
---
+++ src/App.js
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const Apps = () => {
+  return (
+    <View>
+      <Text>Apps</Text>
+    </View>
+  );
+};
+
+export default Apps;
